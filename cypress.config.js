9cc67685f09de5a751b2e63e0c40a0996254176b
omeryttnc <67800186+omeryttnc@users.noreply.github.com>
2023-03-03 21:38:53 +0000

    first commit

---
+++ cypress.config.js
@@ -0,0 +1,46 @@
+const { defineConfig } = require("cypress");
+const mysql = require('mysql')
+
+function queryTestDb(query, config) {
+
+  const connection = mysql.createConnection(config.env.db)
+
+  connection.connect()
+
+  return new Promise((resolve, reject) => {
+
+    connection.query(query, (error, result) => {
+
+      if (error) reject(error)
+      else {
+        connection.end()
+        return resolve(result)
+      }
+    })
+  })
+}
+module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  hideXHR: true,
+  e2e: {
+    watchForFileChanges: false,
+
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+
+      // implement node event listeners here
+    },
+  },
+
+  env:{
+    db: {
+
+      host: "127.0.0.1",
+      port:3306,
+      user: "sqluser",
+      password: "password",
+      database: "school"
+
+    }
+  }
+});
